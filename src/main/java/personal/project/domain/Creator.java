1fcfeb8d259c1ffa641f991f071b4302bc0a75ed
João Guilherme <joao.guilherme@clickcode.dev>
2024-01-04 00:44:52 -0300

    create entities Creator and Document

---
+++ src/main/java/personal/project/domain/Creator.java
@@ -0,0 +1,94 @@
+package personal.project.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Creator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public LocalDateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDateTime birthday) {
+        this.birthday = birthday;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    private String email;
+    private String password;
+
+    public Creator(Long id, String username, String email, String password, String about, LocalDateTime birthday, Document document) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.about = about;
+        this.birthday = birthday;
+        this.document = document;
+    }
+
+    public Creator() {}
+
+    private String about;
+    private LocalDateTime birthday;
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "document_id", referencedColumnName = "id")
+    private Document document;
+
+}
---
+++ src/main/java/personal/project/domain/Document.java
@@ -0,0 +1,63 @@
+package personal.project.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Document {
+    public Document(Long id, Long document, DocumentType type, Creator creator) {
+        this.id = id;
+        this.document = document;
+        this.type = type;
+        this.creator = creator;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getDocument() {
+        return document;
+    }
+
+    public void setDocument(Long document) {
+        this.document = document;
+    }
+
+    public DocumentType getType() {
+        return type;
+    }
+
+    public void setType(DocumentType type) {
+        this.type = type;
+    }
+
+    public Creator getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Creator creator) {
+        this.creator = creator;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long document;
+
+    private DocumentType type;
+
+    @OneToOne(mappedBy = "document")
+    @JoinColumn(name = "creator_id")
+    private Creator creator;
+}
+
+public enum DocumentType {
+    CPF, RG, CNPJ;
+}
