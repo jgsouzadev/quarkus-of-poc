3b7dc5924e7e0aef22c9d98006d067ab56ec1075
João Guilherme <joao.guilherme@clickcode.dev>
2024-01-04 00:56:03 -0300

    return a entity

--- src/main/java/personal/project/GreetingResource.java
+++ src/main/java/personal/project/GreetingResource.java
@@ -4,13 +4,18 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import personal.project.domain.Creator;
+import personal.project.domain.Document;
+import personal.project.domain.enums.DocumentType;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Document hello() {
+        return new Document(
+                1L, 12345678910L, DocumentType.CPF, new Creator()
+        );
     }
 }
